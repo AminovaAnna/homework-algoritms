98a565b726a7bac486f9f1fbabb88e656029c1b9
AminovaAnna <aminovaanna163@gmail.com>
2024-03-24 11:03:19 +0300

    дз

--- src/main/java/pro/sky/algorithms/IntegerArrayList.java
+++ src/main/java/pro/sky/algorithms/IntegerArrayList.java
@@ -207,6 +207,32 @@ public class IntegerArrayList implements IntegerList {
         }
         return false;
     }
+    public static void quickSort(Integer[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                swapElements(arr, i, j);
+            }
+        }
+
+        swapElements(arr, i + 1, end);
+        return i + 1;
+    }
+
+
 }
 
 
